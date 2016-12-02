8eb7db3cf3ab046063aa010e46f4e5bfad5d8656
Alan Watson <alan@Mollies-MacBook-Pro.local>
2016-12-02 13:46:18 -0500

    Commit message

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,23 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne= "queen";
-var cardTwo= "queen";
-var cardThree= "king";
-var cardFour= "king";
+// console.log("JS file is connected to HTML! Woo!")
+// var cardOne= "queen";
+// var cardTwo= "queen";
+// var cardThree= "king";
+// var cardFour= "king";
+
+//  if (cardTwo=== cardFour){
+// 	alert('You found a match!');
+// }
+// else if (cardTwo !== cardFour){
+// 	alert('Sorry, try again.')
+// }
+function createCards(){
+	var gameBoard = document.getElementById('game-board');
+
+	for (var i = 0; i<4 ; i++){
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}
+}
+
+createCards();
